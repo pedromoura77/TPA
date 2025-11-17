f5b42c2355b08930c6c2a47902edf7e313a25b38
pedromoura77 <pedromouraoliveiraa@gmail.com>
2025-11-17 11:12:26 -0300

    Intersecao.java

--- ListaVetores/Intersecao.java
+++ ListaVetores/Intersecao.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Intersecao {
 	public static void main(String[] args) {
 		
-		
-		Scanner in = new Scanner (System.in);
+		Scanner ler = new Scanner (System.in);
 		
 		final int TAM=10;
 		int a[],b[],c[],j,k,contC = 0,repeticoes,i;
@@ -19,13 +18,11 @@ public class Intersecao {
             a[i] = in.nextInt();
         }
 
-        
         for (i = 0; i < TAM; i++) {
             System.out.print("Digite o " + (i+1) + "º valor do vetor B: ");
             b[i] = in.nextInt();
         }
 
-        
         for (i = 0; i < TAM; i++) {
             for (j = 0; j < TAM; j++) {
 
@@ -48,8 +45,7 @@ public class Intersecao {
                 }
             }
         }
-
-        
+ 
         System.out.println("\nVetor C (Interseção):");
         if (contC == 0) {
             System.out.println("Não há elementos em comum.");
@@ -61,3 +57,4 @@ public class Intersecao {
 		
 	}
 }
+
